5dfd70815b351984692a60cf6d15dbe7d987cdcf
Shehan Fernando <shehanFernando@users.noreply.github.com>
2018-07-18 12:10:06 -0400

    cs

---
+++ decision.py
@@ -0,0 +1,16 @@
+x = int(input("Enter x: "))
+y = int(input("Enter Y: "))
+
+if(x==y):
+    print("X & Y are equal")
+
+elif(x != y):
+    if(x < y):
+        print("x is less than y")
+
+    else:
+        print("y is less than x")
+
+else:
+    print("x and y neither equal nor y is greater")#this line will never run
+
